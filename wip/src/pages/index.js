8d2e6cfc8e1c2ceab8afc0a6c671680b1644a374
dosocob <noob100slayer@gmail.com>
2023-03-27 11:30:18 -0700

    fixed bugs

--- wip/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Aboutme} from './aboutme/aboutme';
-export {default as Mlp} from './mlp/mlp';
-export {default as Pas} from './pas/pas';
-export {default as Frequencies} from './frequencies/frequencies';
-
